a9cbc16f3c8e483cd367a5aef3dc7bc833c652a0
Rian <rian.sj@gmail.com>
2017-09-07 22:24:51 +0700

    add src/fibonnaci.js file

---
+++ src/fibonnaci.js
@@ -0,0 +1,37 @@
+// condition: last febo is less than num
+function febo(num) {
+	var febo = [1, 1];
+	var i = 2;
+	var sum = febo[i - 2] + febo[i - 1];
+
+	while (sum < num) {
+		febo.push(sum);
+		i = i + 1;
+		sum = febo[i - 2] + febo[i - 1];
+	}
+
+	return febo;	
+}
+
+
+// condition: sum of odd febo is less than num
+function febo1(num) {
+	var febo = [1, 1];
+	var i = 2;
+	var sum = next = febo[0] + febo[1];
+
+	while (next <= num) {
+		febo.push(next);
+		i = i + 1;
+		var next = febo[i - 2] + febo[i -1];
+
+		if ((next % 2) !== 0) {
+			sum = sum + next;
+		}
+	}
+
+	console.log(febo);
+	console.log(sum);
+}
+
+febo(100);
